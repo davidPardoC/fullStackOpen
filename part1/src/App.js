05aff045a7e695aabc968c4feae6c145d5b8c8b2
David Pardo <pardodavid10@gmail.com>
2022-06-26 22:28:45 -0500

    Part 1: 1.5: course information step5

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,33 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    { name: "Fundamentals of React", excersices: 10 },
-    { name: "Using props to pass data", excersices: 7 },
-    { name: "State of a component", excersices: 14 },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", excersices: 10 },
+      { name: "Using props to pass data", excersices: 7 },
+      { name: "State of a component", excersices: 14 },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
 
 const Header = ({ course }) => {
-  return <h1>{course}</h1>;
+  return <h1>{course.name}</h1>;
 };
 
 const Total = ({ parts }) => {
-  return <p>Number of exercises {parts[0].excersices + parts[1].excersices + parts[2].excersices}</p>;
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts[0].excersices + parts[1].excersices + parts[2].excersices}
+    </p>
+  );
 };
 
 const Content = ({ parts }) => {
