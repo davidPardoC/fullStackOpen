a65b17a619b46d181377774836b57c089870b3ee
davidPardoC <pardodavid10@gmail.com>
2022-07-30 14:37:20 -0500

    4.2 Blog list, step2

---
+++ part4/bloglist-backend/src/app.js
@@ -0,0 +1,15 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
