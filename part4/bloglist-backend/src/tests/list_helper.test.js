034e3d964b1fd97d22e8f50b5842c7317654e7be
davidPardoC <pardodavid10@gmail.com>
2022-07-30 16:03:59 -0500

    4.4: helper functions and unit tests, step2

--- part4/bloglist-backend/src/tests/list_helper.test.js
+++ part4/bloglist-backend/src/tests/list_helper.test.js
@@ -1,8 +1,27 @@
 const listHelper = require('../utils/list_helper')
+const { blogsStub } = require('./stubs/bloglist.stub')
+describe('Dummy', () => {
+  test('dummy returns one', () => {
+    const blogs = []
 
-test('dummy returns one', () => {
-  const blogs = []
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
+})
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const blogs = []
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(0)
+  })
+  test('when list has only one blog, equals the likes of that', () => {
+    const listWithOneBlog = [blogsStub[0]]
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(blogsStub[0].likes)
+  })
+  test('of a bigger list is calculated rigth', () => {
+    const result = listHelper.totalLikes(blogsStub)
+    expect(result).toBe(36)
+  })
 })
--- part4/bloglist-backend/src/utils/list_helper.js
+++ part4/bloglist-backend/src/utils/list_helper.js
@@ -4,4 +4,16 @@ const dummy = (blogs = []) => {
   }
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs = []) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  if (blogs.length === 1) {
+    return blogs[0].likes
+  }
+  const reducer = (prevValue, currentValue) => prevValue + currentValue
+  const result = blogs.map((blog) => blog.likes).reduce(reducer, 0)
+  return result
+}
+
+module.exports = { dummy, totalLikes }
