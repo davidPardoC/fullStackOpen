4cee4826320ddac61bf2a1d6bd56892431e6497a
davidPardoC <pardodavid10@gmail.com>
2022-08-06 20:28:14 -0500

    5.7* Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,31 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+import { useRef } from "react";
+import "./Blog.css";
+import Togglable from "./Togglable";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  const toggleRef = useRef(null);
+  const toggle = () => {
+    toggleRef.current.toggleVisisble();
+    setVisible(!toggleRef.current.visible);
+  };
+  return (
+    <div className="blog">
+      <div>
+        {blog.title}{" "}
+        <button onClick={toggle}>{visible ? "hide" : "show"}</button>
+      </div>
+      <Togglable ref={toggleRef} showButton={false}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>{blog.url}</div>
+          <div>
+            likes: {blog.likes} <button>like</button>
+          </div>
+        </div>
+      </Togglable>
+    </div>
+  );
+};
+
+export default Blog;
--- part5/bloglist-frontend/src/components/Togglable.js
+++ part5/bloglist-frontend/src/components/Togglable.js
@@ -4,34 +4,44 @@ import { useRef } from "react";
 import { useState } from "react";
 import "./Togglable.css";
 
-const Togglable = forwardRef(({ children, label = "" }, refs) => {
-  const ref = useRef();
-  const [childHeight, setChildHeight] = useState(0);
-  const [visible, setVisible] = useState(false);
+const Togglable = forwardRef(
+  ({ children, label = "", showButton = true }, refs) => {
+    const ref = useRef();
+    const [childHeight, setChildHeight] = useState(0);
+    const [visible, setVisible] = useState(false);
 
-  const toggleVisisble = () => {
-    setVisible(!visible);
-  };
-  useEffect(() => {
-    const childHeightRaw = ref.current?.clientHeight + 20;
-    const childHeight = `${childHeightRaw / 16}rem`;
-    setChildHeight(childHeight);
-  }, []);
-
-  useImperativeHandle(refs, () => {
-    return {
-      toggleVisisble,
+    const toggleVisisble = () => {
+      setVisible(!visible);
     };
-  });
+    useEffect(() => {
+      const childHeightRaw = ref.current?.clientHeight + 50;
+      const childHeight = `${childHeightRaw / 16}rem`;
+      setChildHeight(childHeight);
+    }, []);
 
-  return (
-    <div>
-      <div className="content" style={{ maxHeight: visible ? childHeight : 0 }}>
-        <div ref={ref}>{children}</div>
+    useImperativeHandle(refs, () => {
+      return {
+        toggleVisisble,
+        visible,
+      };
+    });
+
+    return (
+      <div>
+        <div
+          className="content"
+          style={{ maxHeight: visible ? childHeight : 0 }}
+        >
+          <div ref={ref}>{children}</div>
+        </div>
+        {showButton && (
+          <button onClick={toggleVisisble}>
+            {!visible ? label : "cancel"}
+          </button>
+        )}
       </div>
-      <button onClick={toggleVisisble}>{!visible ? label : "cancel"}</button>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default Togglable;
