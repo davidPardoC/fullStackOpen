8476325d165757881e28f816a5c298607dfee3c2
davidPardoC <pardodavid10@gmail.com>
2022-08-07 14:54:34 -0500

    5.15: Blog list tests, step3

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import blogService from '../services/blogs'
 import Blog from './Blog'
 
 const renderBlog = (blog, onDelete) => {
@@ -31,5 +32,20 @@ describe('Blog Component', () => {
     await user.click(showButton)
     expect(component.queryByText(mockBlog.likes)).toBeDefined()
     expect(component.queryByText(mockBlog.url)).toBeDefined()
+  })
+
+  it('Should show details on click show and click like button twice', async () => {
+    const updateSpy = jest
+      .spyOn(blogService, 'updateBlog')
+      .mockResolvedValue(mockBlog)
+    const mockOnDelete = jest.fn()
+    const component = renderBlog(mockBlog, mockOnDelete)
+    const user = userEvent.setup()
+    const showButton = await component.findByText('show')
+    await user.click(showButton)
+    const likeButton = await component.findByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+    expect(updateSpy).toHaveBeenCalledTimes(2)
   })
 })
