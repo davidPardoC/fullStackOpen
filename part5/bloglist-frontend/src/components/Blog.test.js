fe12b6fbd5e20fabc02488e0c91326472fd0895d
davidPardoC <pardodavid10@gmail.com>
2022-08-07 14:48:33 -0500

    5.14: Blog list tests, step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 const renderBlog = (blog, onDelete) => {
@@ -7,17 +8,28 @@ const renderBlog = (blog, onDelete) => {
   return rendererComponent
 }
 
+const mockBlog = {
+  title: 'Test Title',
+  author: 'Test Author',
+  likes: 1,
+  url: 'www.test.com',
+}
+
 describe('Blog Component', () => {
   it('Should render basic component', () => {
-    const mockBlog = {
-      title: 'Test Title',
-      author: 'Test Author',
-      likes: 1,
-      url: 'www.test.com',
-    }
     const mockOnDelete = jest.fn()
     const component = renderBlog(mockBlog, mockOnDelete)
     expect(component.queryByText(mockBlog.title)).toBeDefined()
     expect(component.queryByText(mockBlog.author)).toBeDefined()
+  })
+
+  it('Should show details on click show', async () => {
+    const mockOnDelete = jest.fn()
+    const component = renderBlog(mockBlog, mockOnDelete)
+    const user = userEvent.setup()
+    const showButton = await component.findByText('show')
+    await user.click(showButton)
+    expect(component.queryByText(mockBlog.likes)).toBeDefined()
+    expect(component.queryByText(mockBlog.url)).toBeDefined()
   })
 })
