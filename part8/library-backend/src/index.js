2efb2c03aff693103499b9320fcc61f1f562cd12
davidPardoC <pardodavid10@gmail.com>
2022-08-16 22:33:31 -0500

    8.2: All books

--- part8/library-backend/src/index.js
+++ part8/library-backend/src/index.js
@@ -89,9 +89,18 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: String!
+    genres: [String!]
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
@@ -99,6 +108,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
