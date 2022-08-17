48356a3756077a85b9e61e99428e48bc11d3bf0e
davidPardoC <pardodavid10@gmail.com>
2022-08-16 22:43:51 -0500

    8.4: Books of an author

--- part8/library-backend/src/index.js
+++ part8/library-backend/src/index.js
@@ -106,7 +106,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String!): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -115,7 +115,10 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      const { author } = args;
+      return books.filter((book) => book.author === author);
+    },
     allAuthors: () => authors,
   },
 };
