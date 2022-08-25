0be253c5af61080cd7c5b2235c44154baa1e9f0b
davidPardoC <pardodavid10@gmail.com>
2022-08-24 23:49:17 -0500

    9.1 Body mass index

---
+++ part9/project/bmiCalculator.ts
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param weigth in kg
+ * @param heigth in cm
+ * @returns the calculated bmi
+ */
+const calculateBmi = (weigth: number, heigth: number) => {
+  const heigthInMeters = heigth / 100;
+  const bmi = weigth / (heigthInMeters * heigthInMeters);
+  let interpretation = "Normal";
+  if (bmi < 18.5) {
+    interpretation = "Below normal weigth";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    interpretation = "Normal weigth";
+  } else if (bmi >= 25 && bmi < 30) {
+    interpretation = "Overweigth";
+  } else if (bmi >= 30 && bmi < 35) {
+    interpretation = "Class I Obesity";
+  } else if (bmi >= 35 && bmi < 40) {
+    interpretation = "Class II Obesity";
+  } else if (bmi >= 40) {
+    interpretation = "Class III Obesity";
+  }
+  return `${interpretation} ${bmi}`;
+};
+
+console.log(calculateBmi(85, 172));
