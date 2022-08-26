e28a0c138b885e288faade1b0ab75aa40c465a7d
davidPardoC <pardodavid10@gmail.com>
2022-08-26 02:38:07 -0500

    9.8: Patientor backend, step1

--- part9/patientor-backend/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
-var PORT = 4000;
-app.listen(PORT, function () {
-    console.log("Server is running on port ".concat(PORT));
-});
