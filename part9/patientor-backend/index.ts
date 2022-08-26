258a6f5fc35954b57ed12b4d32b79193859b04e0
davidPardoC <pardodavid10@gmail.com>
2022-08-26 02:45:39 -0500

    9.9: Patientor backend, step2

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3001;
 import cors from "cors";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
 
 app.get("/api/ping", (_req, res) => {
